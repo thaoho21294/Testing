1bda7bed8310e787e3c0f99576e7f2f795df4a75
thaoho21294 <thaoho21294@gmail.com>
2016-01-04 07:01:49 +0700

    Add new project testing

---
+++ QuanLyCuaHangLinhKienMayTinh/Testing/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BLL.Warehouse;
+using NUnit.Framework;
+namespace Testing
+{
+    [TestFixture]
+    public class BllProductTest
+    {
+        [Test]
+        public void GetListProduct1()
+        {
+            BllProduct bll = new BllProduct();
+            Assert.IsNull(bll.GetListProducts());
+        }
+
+        [Test]
+        public void GetListProduct2()
+        {
+            BllProduct bll = new BllProduct();
+            Assert.IsNotNull(bll.GetListProducts());
+            ;
+        }
+    }
+}
